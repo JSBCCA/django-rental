af9861924a7ea86013fb7376e403ce1fa6ee322d
jsbcca <jsibert@basecampcodingacademy.org>
2017-04-03 13:25:25 -0500

    Fixed links on both pages

--- rental_app/urls.py
+++ rental_app/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from . import views
 
+app_name = 'django_rental'
+
 urlpatterns = [
     url(r'^about/$', views.about, name='about'),
     url(r'^rent/$', views.rent, name='rent'),
