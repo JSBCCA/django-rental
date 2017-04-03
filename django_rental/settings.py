f02e4543a8c1d90eea3c3752dd24797d906b203f
jsbcca <jsibert@basecampcodingacademy.org>
2017-04-03 13:02:56 -0500

    Fixed urls

--- django_rental/settings.py
+++ django_rental/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'rental_app.apps.RentalAppConfig',
+    'rental_app.apps.RentalAppConfig',
     
 ]
 
