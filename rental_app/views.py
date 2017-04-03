ece1b61f621b0ca4d9a45450ae0015241753cf30
jsbcca <jsibert@basecampcodingacademy.org>
2017-04-03 15:57:33 -0500

    Restructured html, can add items from admin

--- rental_app/views.py
+++ rental_app/views.py
@@ -1,8 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Item
 
 # Create your views here.
 def about(request):
     return render(request, 'rental_app/about.html', {})
 
 def rent(request):
+    items = Item.objects.all()
     return render(request, 'rental_app/rent.html', {})
+
+def rented(request, id):
+    item = Item.objects.get(pk=id)
+    item.quantity -= 1
+    item.save()
+    return redirect('django_page:rent')
