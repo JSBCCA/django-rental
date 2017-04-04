130ffc33371ffc628f5c4a7beba011638251b7d5
jsbcca <jsibert@basecampcodingacademy.org>
2017-04-04 14:57:06 -0500

    Message pop up on rent/return

--- rental_app/views.py
+++ rental_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Item
+from django.contrib import messages
 
 # Create your views here.
 def about(request):
@@ -11,12 +12,15 @@ def rent(request):
 
 def rented(request, id):
     item = Item.objects.get(pk=id)
-    item.num_in_stock -= 1
+    if item.num_in_stock > 0:
+        item.num_in_stock -= 1
+        messages.success(request, "You have rented " + item.name + "!")
     item.save()
     return redirect('django_rental:rent')
 
 def returned(request, id):
     item = Item.objects.get(pk=id)
     item.num_in_stock += 1
+    messages.success(request, "You have returned " + item.name + "!")
     item.save()
     return redirect('django_rental:rent')
