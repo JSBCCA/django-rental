b2f2c3c249e53b3310e339cc9fa6659c5dce2d44
jsbcca <jsibert@basecampcodingacademy.org>
2017-04-04 15:34:51 -0500

    Added images, changed url field to char field

--- rental_app/models.py
+++ rental_app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Item(models.Model):
     name = models.CharField(max_length=200)
-    image_url = models.URLField(max_length=200)
+    image_url = models.CharField(max_length=200)
     price = models.IntegerField(max_length=200)
     num_in_stock = models.IntegerField(max_length=200)
     max_stock = models.IntegerField(max_length=200)
